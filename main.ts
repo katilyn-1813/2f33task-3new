a6b4042abcd197bce38718cc1d282c5a91132d66
katilyn-1813 <115199291+katilyn-1813@users.noreply.github.com>
2022-10-14 09:19:57 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,50 @@
+let y = 0
+input.onButtonPressed(Button.A, function () {
+    y = 9
+    for (let index = 0; index < 10; index++) {
+        basic.showNumber(y)
+        y += -1
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . . . .
+        # . . . #
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
